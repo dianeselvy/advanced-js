d5bdce5efddfdcd61081d8a5e906a4bcc9b63bbd
Diane Selvy <dianeselvy@hotmail.com>
2017-08-17 23:31:06 +0000

    Adds API routes

--- index.js
+++ index.js
@@ -11,28 +11,45 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 app.use(require("body-parser").urlencoded({extended: true}));
 app.use(require("body-parser").json());
+app.use('/api', require("cors")());
+app.use((err, req, res, next) => {
+  console.log(err);
+});
 
 let handlebars =  require("express-handlebars");
 app.engine(".html", handlebars({extname: '.html'}));
 app.set("view engine", ".html");
 var albumSchema = require('./models/albumSchema.js');
 
-            app.get('/', (req, res) => {
+            app.get('/', (req, res, next) => {
                res.type('text/html');
                Radiohead.find((err, result) => {
-                    // output error if one occurred
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        // otherwise output the array of documents
-                        res.render("home", {albums: result});
-                    }
+                    console.log(result);
+            if (err) return next(err);
+                        res.render("home", {'albums': {albums: JSON.stringify(result)}});
+                    
                 });
             });
               
             app.get('/about', (req, res) => {
              res.type('text/plain');
              res.send('About page');
+            });
+            
+            app.get('/api/v1/add/:name', (req,res,next) => {
+                let name = req.params.name;
+                console.log(name);
+                Radiohead.findOne({name: name}, (err, result) =>{
+                    if (err || !result) return next(err);
+                    res.json(result);
+                });
+            });
+            
+            app.get('/api/v1/radiohead', (req,res,next) => {
+                Radiohead.find((err, results) => {
+                    if (err || !results) return next(err);
+                    res.json(results);
+                });
             });
             
             app.post('/detail', function(req,res){
@@ -50,19 +67,21 @@ var albumSchema = require('./models/albumSchema.js');
                 });
             });
             
-            app.get('/delete', function(req,res){
-              console.log(req.query);
+            app.get('/api/v1/delete/:id', (req,res,next) => {
               //let result = Radiohead.deleteAlbum(req.query.name);
-              Radiohead.remove({ name: req.query.name },(err, result) => {
-                    console.log(result);
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        // otherwise output the array of documents
-                        res.render("delete", {name: req.query.name, result});
-                    }
+              Radiohead.remove({"_id":req.params.id},(err, result) => {
+                    if (err) return next(err);
+                    res.json({"deleted": result.result.n});
                 });
             });
+            
+            app.get('/api/v1/add/:name/:songs/:year', (req,res,next) => {
+                let name = req.params.name;
+                Radiohead.update({name:name}, {name:name, songs:req.params.songs, year:req.params.songs}, {upsert: true}, (err, result) => {
+                    if (err) return next(err);
+                    res.json({updated: result.nModified});
+                })
+            })
               
             app.use( (req,res) => {
              res.type('text/plain'); 
